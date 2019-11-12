40605658b8e37cd15b308fe6804e9f4c4f49a3fe
Randall Phillips <rphill45@uncc.edu>
2019-11-11 19:21:07 -0500

    added prompts

--- index.js
+++ index.js
@@ -12,8 +12,24 @@ inquirer
         message: "What is your GitHub Username?", 
         name: "username"
       },
+      {
+        type: "password",
+        message: "What is your password?", 
+        name: "password"
+      },
+      {
+        type: "password",
+        message: "Re-enter password to confirm:", 
+        name: "confirm"
+      },
      
     ])
     .then(function(response){
         console.log(response);
+        if (response.confirm === response.password){
+            console.log("Success!");
+        } 
+        else {
+            console.log("Your passwords don't match!");
+        }
   });
