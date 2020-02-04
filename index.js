4d884a352cb902c70e4d677fdb3b054e1c47bf2e
Randall Phillips <rphill45@uncc.edu>
2020-02-03 20:37:43 -0500

    installed axios and fs to call to github

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
+var axios = require("axios");
 var inquirer = require("inquirer");
+var fs = require("fs");
 
-inquirer 
+inquirer
   .prompt([
-      {
-        type: "input", 
-        message: "What's your favorite color?",
-        name: "color"
-      }, 
-      {
-        type: "input",
-        message: "What is your GitHub Username?", 
-        name: "username"
-      },
-      {
-        type: "password",
-        message: "What is your password?", 
-        name: "password"
-      },
-      {
-        type: "password",
-        message: "Re-enter password to confirm:", 
-        name: "confirm"
-      },
-     
-    ])
-    .then(function(response){
-        console.log(response);
-        if (response.confirm === response.password){
-            console.log("Success!");
-        } 
-        else {
-            console.log("Your passwords don't match!");
-        }
+    {
+      type: "input",
+      message: "What is your gitHub username?",
+      name: "username"
+    },
+    {
+      type: "list",
+      message: "What is your favorite color?",
+      name: "color",
+      choices: ["Red", "Blue", "Orange", "Green", "Purple", "Yellow"]
+    }
+  ])
+  .then(function(answers) {
+    axios
+      .get("https://api.github.com/users/" + answers.username)
+      .then(function(response) {
+        fs.writeFile(
+          "myPage.md",
+          generateMD({
+            name: response.data.login,
+            favColor: answers.color,
+            imgURL: response.data.avatar_url,
+            repoLink: response.data.html_url,
+            location: response.data.location,
+            bio: response.data.bio,
+            repos: response.data.public_repos,
+            followers: response.data.followers,
+            following: response.data.following
+          }),
+          function(err) {
+            if (err) throw err;
+            console.log("MD generated!");
+          }
+        );
+      });
   });
+
+function generateMD(data) {
+  return `## <h1 style="color:${data.favColor}">${data.name}</h1>
+# ![UserProfileImage](${data.imgURL})
+    
+    
+    
+Repo: <a href="${data.repoLink}">${data.name}'s GitHub Profile</a>.
+    
+        
+    Location:  ${data.location}    
+    
+    
+    Bio:  ${data.bio} 
+    
+    
+    Public Repos:  ${data.repos} 
+    
+    
+    Followers:  ${data.followers}  
+    
+    
+    Following:  ${data.following} `;
+}
---
+++ myPage.md
@@ -0,0 +1,21 @@
+## <h1 style="color:Blue">rphill26</h1>
+# ![UserProfileImage](https://avatars0.githubusercontent.com/u/54990905?v=4)
+    
+    
+    
+Repo: <a href="https://github.com/rphill26">rphill26's GitHub Profile</a>.
+    
+        
+    Location:  null    
+    
+    
+    Bio:  null 
+    
+    
+    Public Repos:  23 
+    
+    
+    Followers:  0  
+    
+    
+    Following:  0 
